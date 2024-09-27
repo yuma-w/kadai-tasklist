e1d1f3f9215ecb8720b697ce43ef1c436b380bf6
yuuma.watanabe2 <iikotoarusa30@gmail.com>
2024-09-28 01:07:11 +0900

    Index/NewServletの修正

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,10 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
+//import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+//import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
-import utils.DBUtil;
+//import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
@@ -32,28 +33,38 @@ public class NewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
-        em.getTransaction().begin();
+        // CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
+
+        // おまじないとしてのインスタンスを生成
+        request.setAttribute("message", new Message());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        rd.forward(request, response);
+
+
+        // EntityManager em = DBUtil.createEntityManager();
+       // em.getTransaction().begin();
 
         // Messageのインスタンスを生成
-        Message m = new Message();
+       // Message m = new Message();
 
         // mの各フィールドにデータを代入
 
-        String content = "hello";
-        m.setContent(content);
+       // String content = "hello";
+       // m.setContent(content);
 
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis());     // 現在の日時を取得
-        m.setCreated_at(currentTime);
-        m.setUpdated_at(currentTime);
+      //  Timestamp currentTime = new Timestamp(System.currentTimeMillis());     // 現在の日時を取得
+      //  m.setCreated_at(currentTime);
+      //  m.setUpdated_at(currentTime);
 
         // データベースに保存
-        em.persist(m);
-        em.getTransaction().commit();
+      //  em.persist(m);
+     //   em.getTransaction().commit();
 
         // 自動採番されたIDの値を表示
-        response.getWriter().append(Integer.valueOf(m.getId()).toString());
+      //  response.getWriter().append(Integer.valueOf(m.getId()).toString());
 
-        em.close();
+      //  em.close();
      }
 }
